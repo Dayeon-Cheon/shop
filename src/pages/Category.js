de97fdea64a2e9d726e3e7a1571b9c34667bf984
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2023-06-27 23:53:07 +0100

    category

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -3,9 +3,35 @@ function Category(props) {
   let cat = queryParameters.get("cat");
 
   // 카테고리 필터
+  console.log(cat);
 
-  //   console.log();
+  let category = "";
 
+  if (cat === "fresh-food") {
+    category = "Fresh food";
+  }
+  if (cat === "food-cupboard") {
+    category = "Food cupboard ";
+  }
+  if (cat === "drinks") {
+    category = "Drinks";
+  }
+
+  console.log(category);
+
+  //   console.log(props.item);
+
+  // const filteredItem = props.item.filter((i) => i.catagory === cat);
+
+  //   const filteredItem = props.item.filter((e) => {
+  //     return e.category === cat;
+  //   });
+
+  let filteredItem = props.item.filter(function (i) {
+    return i.category === cat;
+  });
+
+  console.log(filteredItem);
   return <div>{cat}</div>;
 }
 
