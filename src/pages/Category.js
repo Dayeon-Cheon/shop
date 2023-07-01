49405e1688db26cb6fcda754c6ddf0305fb53d55
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2023-07-01 22:40:05 +0100

    category

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -7,19 +7,18 @@ function Category(props) {
     return i.category === cat;
   });
 
-  //   console.log(filteredItem);
+  console.log(filteredItem);
 
   filteredItem.map(function (a, i) {
     return (
-      <div>hi</div>
-      //   <div className="col-md-4">
-      //     <img
-      //       className="item-img-main"
-      //       src={process.env.PUBLIC_URL + "/img/" + props.picNum + ".jpg"}
-      //     />
-      //     <h4>{props.item.title}</h4>
-      //     <p>£{props.item.price}</p>
-      //   </div>
+      <div className="col-md-4">
+        <img
+          className="item-img-main"
+          src={process.env.PUBLIC_URL + "/img/" + filteredItem.id + ".jpg"}
+        />
+        <h4>{filteredItem.title}</h4>
+        <p>£{filteredItem.price}</p>
+      </div>
     );
   });
 }
